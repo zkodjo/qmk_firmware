cb421c473ac64a1214a895e6e5b574f90c0f025b
Zach Kodjo <36774583+kodjoz@users.noreply.github.com>
2025-05-07 21:27:10 -0400

    keymap changes day 2

--- keyboards/hotdox76v2/config.h
+++ keyboards/hotdox76v2/config.h
@@ -21,8 +21,8 @@
 //#define NO_ACTION_ONESHOT
 #define EE_HANDS
 #define SPLIT_TRANSACTION_IDS_KB KEYBOARD_CURRENT_ALPA_SYNC
-#define DYNAMIC_KEYMAP_LAYER_COUNT 6
-#define DYNAMIC_KEYMAP_MACRO_COUNT 8
+// #define DYNAMIC_KEYMAP_LAYER_COUNT 6
+// #define DYNAMIC_KEYMAP_MACRO_COUNT 8
 #define LAYER_STATE_8BIT
 
 /* remove these items for space-saving */
@@ -31,13 +31,58 @@
 #define NO_MUSIC_MODE
 #define NO_ACTION_ONESHOT
 
+/* from yandrstudio firmware */
+#define FORCE_NKRO
+#define DEBOUNCE 5
+#define USB_POLLING_INTERVAL_MS 1
+
+#define TAP_CODE_DELAY 15
+
+
+/* from yandrstudio configure keyboard sync */
+#ifdef SPLIT_KEYBOARD
+
+// #   define MASTER_RIGHT
+
+#   define USE_SERIAL
+
+#   define SOFT_SERIAL_PIN D2
+
+#   define SPLIT_TRANSPORT_MIRROR
+
+// #   define SPLIT_USB_DETECT
+
+#   define SPLIT_MODS_ENABLE
+#   define SPLIT_LAYER_STATE_ENABLE
+#   define SPLIT_LED_STATE_ENABLE
+#   define SPLIT_OLED_ENABLE
+#   define SPLIT_TRANSACTION_IDS_KB KEYBOARD_CURRENT_ALPA_SYNC
+
+#endif
+
+#ifdef RGBLIGHT_ENABLE
+
+#    define RGBLIGHT_SPLIT
+#    define RGBLED_SPLIT {43, 43}
+#    define RGB_DI_PIN D3
+#    define RGBLED_NUM (43*2)
+#    define DRIVER_LED_TOTAL RGBLED_NUM
+#    define RGBLIGHT_LIMIT_VAL 150
+#    define RGBLIGHT_SLEEP
+#    define WS2812_TRST_US 100
+// #    define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
+#endif
+
 // fixes Macro tapping term
 #undef TAPPING_TERM
 
 #define TAPPING_TERM 220
+#define QUICK_TAP_TERM 0
 
 /* RGB Animations */
 #define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_DIGITAL_RAIN
+#define ENABLE_RGB_MATRIX_RIVERFLOW
 #define RGB_MATRIX_DEFAULT_HUE 0
 #define RGB_MATRIX_DEFAULT_VALUE 75
 #define RGB_MATRIX_DEFAULT_SAT 255
--- keyboards/hotdox76v2/keymaps/zkodjo/keymap.c
+++ keyboards/hotdox76v2/keymaps/zkodjo/keymap.c
@@ -4,6 +4,18 @@
 
 #include QMK_KEYBOARD_H
 
+// Left-hand home row mods
+#define HOME_A LGUI_T(KC_A)
+#define HOME_S LALT_T(KC_S)
+#define HOME_D LCTL_T(KC_D)
+#define HOME_F LSFT_T(KC_F)
+
+// Right-hand home row mods
+#define HOME_J RSFT_T(KC_J)
+#define HOME_K RCTL_T(KC_K)
+#define HOME_L LALT_T(KC_L)
+#define HOME_SCLN RGUI_T(KC_SCLN)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌──────┬───┬───┬───┬───┬───┬───┐                     ┌───┬───┬───┬───┬───┬───┬──────┐
@@ -25,9 +37,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ergodox_pretty(
         KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_LEFT,             KC_RIGHT,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_EQL,             KC_Y,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_CAPS,  MT(MOD_LGUI,KC_A),    MT(MOD_LALT,KC_S),    MT(MOD_LCTL,KC_D),    MT(MOD_LSFT,KC_F),    KC_G,                                 KC_H,    MT(MOD_RSFT,KC_J),    MT(MOD_RCTL,KC_K),    MT(MOD_LALT,KC_L),    MT(MOD_RGUI,KC_SCLN), KC_QUOT,
-        MOD_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,               KC_N,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        MOD_LCTL, KC_GRV,   CW_TOGG,   KC_LGUI, LT(2, KC_ESC),                                                LT(5, KC_DEL), KC_DOWN, KC_LEFT_BRACKET,   KC_RIGHT_BRACKET, KC_RCTL,
+        KC_CAPS,  HOME_A,    HOME_S,    HOME_D,    HOME_F,    KC_G,                                 KC_H,    HOME_J,    HOME_K,    HOME_L,    HOME_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_B,               KC_N,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+        KC_LCTL, KC_GRV,   CW_TOGG,   KC_LGUI, LT(2, KC_ESC),                                                LT(5, KC_DEL), KC_DOWN, KC_LEFT_BRACKET,   KC_RIGHT_BRACKET, KC_RCTL,
                                                      KC_NO,    KC_NO,               KC_NO, KC_NO,
                                                               KC_NO,            KC_NO,
                                             LT(1, KC_SPC), KC_TAB, KC_NO,             KC_NO, LT(4, KC_ENT), LT(3, KC_BSPC)
@@ -36,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [1] = LAYOUT_ergodox_pretty(
         KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,             KC_NO,    KC_NO,    KC_NO,    KC_8,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,             KC_NO,    KC_NO,    RCTL(KC_V),    RCTL(KC_C),    RCTL(KC_X),    RCTL(KC_Z),    KC_NO,
-        KC_NO,  MOD_LGUI,    MOD_LALT,    MOD_LCTL,    MOD_LSFT,    KC_NO,             KC_CAPS,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, KC_NO,
+        KC_NO,  KC_LGUI,    KC_LALT,    KC_LCTL,    KC_LSFT,    KC_NO,             KC_CAPS,    KC_LEFT,    KC_DOWN,    KC_UP,    KC_RIGHT, KC_NO,
         KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,               KC_NO,    KC_INS,    KC_HOME,    KC_PGDN, KC_PGUP,  KC_END, KC_NO,
         KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,                                                KC_DEL, KC_NO, KC_NO,   KC_NO, KC_NO,
                                                      KC_NO,    KC_NO,               KC_NO, KC_NO,
@@ -47,7 +59,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT_ergodox_pretty(
         KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,             KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,  KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,             KC_NO,    RM_TOGG,    RM_NEXT,    RM_HUEU,    RM_SATU,    RM_VALU,    RM_SPDU,
-        KC_NO,  MOD_LGUI,    MOD_LALT,    MOD_LCTL,    MOD_LSFT,  KC_NO,              KC_NO,    KC_MPRV,    KC_VOLD,    KC_VOLU, KC_MNXT, KC_NO,    
+        KC_NO,  KC_LGUI,    KC_LALT,   KC_LCTL,    KC_LSFT,  KC_NO,              KC_NO,    KC_MPRV,    KC_VOLD,    KC_VOLU, KC_MNXT, KC_NO,    
         KC_NO, KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,    KC_NO,               KC_NO,    KC_NO,    RM_PREV,    RM_HUED, RM_SATD,  RM_VALD, RM_SPDD,
         KC_NO, KC_NO,   KC_NO,   KC_NO, KC_NO,                                                KC_NO, KC_NO, KC_NO,   KC_NO, KC_NO,
                                                      KC_NO,    KC_NO,               KC_NO, KC_NO,
